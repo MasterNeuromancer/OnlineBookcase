a1965b6a251a1114f34847e1da28dd246216f5d6
Matthew Elliott <mse951993@gmail.com>
2019-03-08 14:29:16 -0800

    added modal with video

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDWe0UI_fJcDGtzqvfFuvHPkbAZddTW9cI",
+    authDomain: "project1-36185.firebaseapp.com",
+    databaseURL: "https://project1-36185.firebaseio.com",
+    projectId: "project1-36185",
+    storageBucket: "project1-36185.appspot.com",
+    messagingSenderId: "112275278683"
+};
+firebase.initializeApp(config);
+
 var apiGoogle = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
 
 var bookISBN = "0451524934";
@@ -16,13 +27,19 @@ $.ajax({
     console.log(response.items[0].volumeInfo.publishedDate);
 });
 
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDWe0UI_fJcDGtzqvfFuvHPkbAZddTW9cI",
-    authDomain: "project1-36185.firebaseapp.com",
-    databaseURL: "https://project1-36185.firebaseio.com",
-    projectId: "project1-36185",
-    storageBucket: "project1-36185.appspot.com",
-    messagingSenderId: "112275278683"
-};
-firebase.initializeApp(config);
+
+var videoElement = $("#video")[0];
+$("#scan-barcode").click(function() {
+    navigator.mediaDevices.getUserMedia({ video: { width: { ideal: 1280 }, height: { ideal: 720 }, facingMode: { ideal: 'environment' } } }).then(function(stream) {
+        videoElement.srcObject = stream;
+        videoElement.play();
+
+    });
+})
+
+$('#exampleModal').on('hidden.bs.modal', function (e) {
+    console.log("CLOSED!")
+    var stream = videoElement.srcObject;
+    var videoTrack = stream.getTracks();
+    videoTrack[0].stop();
+  })
