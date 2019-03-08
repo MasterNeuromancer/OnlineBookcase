3ead73b4e7ba00e043a4feefc212539b84cd2c2e
Neuromancer93 <itrejonumber1@gmail.com>
2019-03-08 13:41:31 -0800

    firebase consolelog working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,17 +4,17 @@ var bookISBN = "0451524934";
 
 var queryURL = apiGoogle + bookISBN;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    console.log(response.items[0].volumeInfo.authors[0]);
-    console.log(response.items[0].volumeInfo.title);
-    console.log(response.items[0].volumeInfo.categories[0]);
-    console.log(response.items[0].volumeInfo.pageCount);
-    console.log(response.items[0].volumeInfo.publishedDate);
-});
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response);
+//     console.log(response.items[0].volumeInfo.authors[0]);
+//     console.log(response.items[0].volumeInfo.title);
+//     console.log(response.items[0].volumeInfo.categories[0]);
+//     console.log(response.items[0].volumeInfo.pageCount);
+//     console.log(response.items[0].volumeInfo.publishedDate);
+// });
 
 // Initialize Firebase
 var config = {
@@ -26,3 +26,19 @@ var config = {
     messagingSenderId: "112275278683"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var testing = "testing"
+
+var userName = "matt"
+
+database.ref(userName).on("child_added", function (snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Console.loging the last user's data
+    console.log(sv.author);
+    console.log(sv.genre);
+    console.log(sv.title);
+});
