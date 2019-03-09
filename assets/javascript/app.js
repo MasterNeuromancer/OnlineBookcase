16dc7f119e535a1e1bb3e30b2101a4aabe765cf4
William Diebolt <william@Williams-MacBook-Pro.local>
2019-03-08 21:30:55 -0800

    book ajax needs to be made a function. db<->ui has been built

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,4 @@
-var apiGoogle = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
-
-var bookISBN = "0451524934";
-
-var queryURL = apiGoogle + bookISBN;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    console.log(response.items[0].volumeInfo.authors[0]);
-    console.log(response.items[0].volumeInfo.title);
-    console.log(response.items[0].volumeInfo.categories[0]);
-    console.log(response.items[0].volumeInfo.pageCount);
-    console.log(response.items[0].volumeInfo.publishedDate);
-});
-
-Initialize Firebase
+/*    FIREBASE BOILERPLATE    */
 var config = {
     apiKey: "AIzaSyDWe0UI_fJcDGtzqvfFuvHPkbAZddTW9cI",
     authDomain: "project1-36185.firebaseapp.com",
@@ -26,26 +8,45 @@ var config = {
     messagingSenderId: "112275278683"
 };
 firebase.initializeApp(config);
-
 var database = firebase.database();
 
+/*	  Find Book URL & Make AJAX Call    */
+var apiGoogle = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+var bookISBN = "0451524934";
+var queryURL = apiGoogle + bookISBN;
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (r) {
+    console.log(r);
+    console.log(r.items[0].volumeInfo.authors[0]);
+    console.log(r.items[0].volumeInfo.title);
+    console.log(r.items[0].volumeInfo.categories[0]);
+    console.log(r.items[0].volumeInfo.pageCount);
+    console.log(r.items[0].volumeInfo.publishedDate);
+
+/*    PUSH BOOK OBJECT TO FIREBASE    */ 
+	database.ref(userName).push({
+		author: r.items[0].volumeInfo.authors[0],
+		genre: r.items[0].volumeInfo.title,
+		title: r.items[0].volumeInfo.categories[0],	
+		pages: r.items[0].volumeInfo.pageCount,
+		date: r.items[0].volumeInfo.publishedDate
+
+	});
+});
+
+
 var testing = "testing"
-
 var userName = "matt"
-
 database.ref(userName).on("child_added", function (snapshot) {
-    // storing the snapshot.val() in a variable for convenience
-    var sv = snapshot.val();
-
-    // Console.loging the last user's data
-
+    var book = snapshot.val();
     var row = $("<tr>");
-    row.append($("<th>").text(sv.author);
-    row.append($("<th>").text(sv.genre);
-    row.append($("<th>").text(sv.title);
+    row.append($("<th>").text(book.author));
+    row.append($("<th>").text(book.genre));
+    row.append($("<th>").text(book.title));
     row.append($("<th>").text("..."));
     row.append($("<th>").text("..."));
     row.append($("<th>").text("..."));
-
     $("#user-data").append(row);
 });
