bf9fe045c5565d05734be4bd4809aa3db297cad1
William Diebolt <william@Williams-MacBook-Pro.local>
2019-03-12 18:07:47 -0700

    abstracted table header handlers into function AND reverse sort works

--- assets/javascript/sort.js
+++ assets/javascript/sort.js
@@ -1,8 +1,46 @@
 console.log("sort.js linked");
 /*	userData is pushed the books from the DB on when page loads, when value are added to DB from ISBN_to_firebase	*/
 
+
+
+
+var sortFlag = false;
+var reverseFlag = false;
+function setupHeaderHandlers(ID, prop){
+	$(ID).click(function() {
+		if(sortFlag !== prop){
+			sort(userData, prop);
+			sortFlag = prop;
+			printTable(userData);
+		}else{
+			if(reverseFlag){
+				printTable(userData); //it's already in reverse, so reverse it again
+				reverseFlag = false;
+			}else{
+				printTable(userData, true); //reverse printing with optional arg
+				reverseFlag = true;
+			}
+		}
+		console.log("sort by"+prop+"clicked");
+	});
+}
+
+
+
+
+setupHeaderHandlers("#title-header", "title");
+setupHeaderHandlers("#author-header", "author");
+setupHeaderHandlers("#genre-header", "genre");
+setupHeaderHandlers("#date-header", "date");
+setupHeaderHandlers("#pages-header", "pages");
+setupHeaderHandlers("#isbn-header", "ISBN");
+setupHeaderHandlers("#snippet-header", "snippet");
+
+/////////////abstract this into function
+/*
 $("#title-header").click(function () {
 	sort(userData, "title");
+	console.log("sort by title clicked!");
 	printTable(userData);
 });
 $("#author-header").click(function () {
@@ -29,6 +67,7 @@ $("#snippet-header").click(function () {
 	sort(userData, "snippet");
 	printTable(userData);
 });
+*/
 
 function sort(array, prop) {
 	for (var i = 0; i < array.length - 1; i++) {
@@ -46,7 +85,7 @@ function sort(array, prop) {
 }
 
 
-function printTable(array) {
+function printTable(array, reverse = false) {
 	$("#table-data").html("");
 	for (var i = 0; i < array.length; ++i) {
 		var row = $("<tr>");
@@ -66,8 +105,7 @@ function printTable(array) {
 		removeBTN.attr("style", "color:red;");
 		removeBTN.attr("value", array[i].key);
 		removeBTN.html("<i class='far fa-trash-alt'>")
-		console.log("Print Table");
-
+//		console.log("Print Table");
 		row.append($("<td>").text(array[i].title));
 		row.append($("<td>").text(array[i].author));
 		row.append($("<td>").text(array[i].genre));
@@ -76,7 +114,13 @@ function printTable(array) {
 		row.append($("<td>").text(array[i].ISBN));
 		row.append($("<td class='text-center'>").html(popover.popover()));
 		row.append($("<td class='text-center'>").html(removeBTN));
-		$("#table-data").append(row);
-
+		
+		if(!reverse){
+			$("#table-data").append(row);
+		}else if(reverse){
+			$("#table-data").prepend(row);
+			
+		}
 	}
+		console.log("printTable called");
 }
